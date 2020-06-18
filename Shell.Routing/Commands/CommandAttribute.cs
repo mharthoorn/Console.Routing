786acca7cf60a3b5f3dc39e480bf67f0461fd14c
Martijn <m.harthoorn@furore.com>
2020-06-18 23:23:36 +0200

    wip: Global settings

--- Shell.Routing/Commands/CommandAttribute.cs
+++ Shell.Routing/Commands/CommandAttribute.cs
@@ -61,4 +61,11 @@ namespace Shell.Routing
     }
 
 
+    // Work in progress. The route binding should strip the arguments the method [Global] before trying to find routing candidates.
+    public class Global : Attribute
+    {
+
+    }
+
+
 }
