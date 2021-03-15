5e959ce9dd0323bc1c10823c94f10efc84b163bb
martijn <martijn@fire.ly>
2021-03-15 12:06:07 +0100

    add: test for optional attribute

---
+++ Console.Routing.Tests/Modules/AttributesModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleRouting.Tests
+{
+    [Module]
+    public class AttributesModule
+    {
+     
+        [Command]
+        public void TryMe([Optional]string name)
+        {
+
+        }
+    }
+}
---
+++ Console.Routing.Tests/TestAttributes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleRouting.Tests
+{
+    [TestClass]
+    public class TestAttributes
+    {
+        Router router = new RouterBuilder()
+            .AddAssemblyOf<TestParameters>()
+            .Build();
+
+        [TestMethod]
+        public void Optionality()
+        {
+            var args = router.Parse("tryme");
+            var result = router.Bind(args);
+            
+            Assert.AreEqual("TryMe", result.Bind.Route.Method.Name);
+        }
+
+    }
+}
