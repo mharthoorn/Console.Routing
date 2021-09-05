369d5a9aac7cd15dd79e1825d95b64f25d0a9375
martijn <martijn@fire.ly>
2021-09-05 23:40:27 +0200

    simplify template and update documentation

--- App/Program.cs
+++ App/Program.cs
@@ -1,11 +1,13 @@
-﻿using ConsoleAppTemplate;
+﻿//using ConsoleAppTemplate;
 using ConsoleRouting;
 
-var router = new RouterBuilder()
-    .AddCurrentAssembly()
-    .AddDefaultHelp()
-    .AddService<Dog>()
-    .AddService<Cow>()
-    .Build();
+//var router = new RouterBuilder()
+//    .AddCurrentAssembly()
+//    .AddDefaultHelp()
+//    .AddService<Dog>()
+//    .AddService<Cow>()
+//    .Build();
 
-router.Handle(args);
+//router.Handle(args);
+
+Routing.Handle(args);
